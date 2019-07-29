9f83c4857a20bac87224208681367e2d25428683
h2ash <hun22ash@gmail.com>
2019-07-29 15:53:08 +0300

    Updates

---
+++ precache-manifest.f4c429ff1ee8c2d9935bea18dbb61464.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68dabbe37ade91f03b6136574fb471c8",
+    "url": "/react_tabs/index.html"
+  },
+  {
+    "revision": "87f5d90a582dcda40136",
+    "url": "/react_tabs/static/css/main.65938976.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react_tabs/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "87f5d90a582dcda40136",
+    "url": "/react_tabs/static/js/main.0ca9d79e.chunk.js"
+  },
+  {
+    "revision": "010ecae327fc4474afd9",
+    "url": "/react_tabs/static/js/runtime~main.310f5775.js"
+  }
+]);
